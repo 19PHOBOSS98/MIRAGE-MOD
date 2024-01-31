9fa71fb7097922e3be40240e02ea05a7e7318cf0
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-01-31 22:43:55 +0800

    added config file

--- common/src/main/java/net/phoboss/mirage/Mirage.java
+++ common/src/main/java/net/phoboss/mirage/Mirage.java
@@ -1,6 +1,12 @@
 package net.phoboss.mirage;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonWriter;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
+import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
 import net.fabricmc.api.EnvType;
@@ -12,11 +18,22 @@ import net.phoboss.mirage.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 public class Mirage {
     public static final String MOD_ID = "mirage";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static Path SCHEMATICS_FOLDER;
+    public static Path CONFIG_FILE;
     public static void init() {
+        initConfigFile();
+        initSchematicsFolder();
+
         ModBlocks.registerAll();
         ModBlockEntities.registerAll();
         ModItems.registerAll();
@@ -37,6 +54,40 @@ public class Mirage {
                 {
                     LOGGER.info("Client stopping!");
                 });
+        }
+    }
+    public static void initFolder(Path path){
+        try {
+            Files.createDirectories(path);
+        }catch(Exception e){
+            LOGGER.error(e.getMessage(),e);
+        }
+    }
+    public static void initConfigFile(){
+        Path configPath = Platform.getConfigFolder().resolve("mirage");
+        initFolder(configPath);
+        CONFIG_FILE = configPath.resolve("mirage_config.json");
+        if(CONFIG_FILE.toFile().exists()){
+            return;
+        }
+        JsonObject mirageConfig = new JsonObject();
+        mirageConfig.addProperty("schematicsDirectoryName", "schematics");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (JsonWriter writer = new JsonWriter(new FileWriter(CONFIG_FILE.toFile()))) {
+            gson.toJson(mirageConfig, mirageConfig.getClass(), writer);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(),e);
+        }
+    }
+    public static void initSchematicsFolder(){
+        try{
+            JsonObject configs = JsonParser.parseReader(new FileReader(CONFIG_FILE.toFile())).getAsJsonObject();
+            String directoryName = configs.get("schematicsDirectoryName").getAsString();
+            SCHEMATICS_FOLDER = Platform.getGameFolder().resolve(directoryName);
+            initFolder(SCHEMATICS_FOLDER);
+        }catch(Exception e){
+            LOGGER.error(e.getMessage(),e);
         }
     }
 }
